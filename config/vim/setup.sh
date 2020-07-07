e9f893d3e7da3826faf9b9aff0ab45094a5192df
Yanhao Yang <yanhao.yang@fromatob.com>
2020-07-07 14:19:08 +0200

    Fixed coc-snippets

--- config/vim/setup.sh
+++ config/vim/setup.sh
@@ -8,10 +8,11 @@ fi
 if [[ ! -d bundle/coc.nvim ]]; then
   mkdir -p $HOME/.config/coc
   git clone https://github.com/neoclide/coc.nvim.git bundle/coc.nvim
-  cd bundle/coc.nvim && yarn install && cd ~/.vim
+  cd bundle/coc.nvim && yarn install
+  mkdir -p $HOME/.config/coc/extensions && cd $HOME/.config/coc/extensions && yarn add coc-snippets
+  cd ~/.vim
 fi
 
 [[ -d bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
 vim --not-a-term -u ./plugins +BundleInstall +qa
 vim --not-a-term -c "execute 'silent GoUpdateBinaries' | execute 'quit'"
-vim --not-a-term -c ":CocInstall coc-snippets | sleep 5000m | qa"
